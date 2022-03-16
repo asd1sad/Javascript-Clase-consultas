3161bf05be0d4716e657078f603683a6ddd7cab0
OCB <93293618+asd1sad@users.noreply.github.com>
2022-03-16 16:34:44 -0300

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,73 @@
+//! 5
+
+let productoIngresado1 = prompt("1er producto de almacen")
+let productoIngresado2 = prompt("2do producto de almacen")
+let productoIngresado3 = prompt("3er producto de almacen")
+let productoIngresado4 = prompt("4to producto de almacen")
+
+
+/* if ((productoIngresado1 != null) && (productoIngresado2 != null) && (productoIngresado3 != null) && (productoIngresado4 != null)) {
+    alert(productoIngresado1 + " " + productoIngresado2 + " " + productoIngresado3 + " " + productoIngresado4);
+} else {
+    alert("Error: Es necesario cargar todos los productos");
+} */
+
+if ((productoIngresado1 != "") && (productoIngresado2 != "") && (productoIngresado3 != "") && (productoIngresado4 != "")) {
+    alert(productoIngresado1 + " " + productoIngresado2 + " " + productoIngresado3 + " " + productoIngresado4);
+} else {
+    alert("Error: Es necesario cargar todos los productos");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let productosIngresados = (productoIngresado1 + productoIngresado2 + productoIngresado3 + productoIngresado4)
+
+
