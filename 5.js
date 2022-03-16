7b32e394efb629e395968a7706f8235fe4fab4d0
OCB <93293618+asd1sad@users.noreply.github.com>
2022-03-16 16:37:22 -0300

    Update 5.js

--- 5.js
+++ 5.js
@@ -5,6 +5,7 @@ let productoIngresado2 = prompt("2do producto de almacen")
 let productoIngresado3 = prompt("3er producto de almacen")
 let productoIngresado4 = prompt("4to producto de almacen")
 
+//! NO ES LO MISMO "" A NULL ?
 
 /* if ((productoIngresado1 != null) && (productoIngresado2 != null) && (productoIngresado3 != null) && (productoIngresado4 != null)) {
     alert(productoIngresado1 + " " + productoIngresado2 + " " + productoIngresado3 + " " + productoIngresado4);
